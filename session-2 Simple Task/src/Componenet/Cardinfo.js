54a4d103dbf8497268a0f0209107fc29338d274d
Azzi-MO <alhwarymostafa@gmail.com>
2022-12-25 08:02:00 -0800

    edit

--- session-2 Simple Task/src/Componenet/Cardinfo.js
+++ session-2 Simple Task/src/Componenet/Cardinfo.js
@@ -18,15 +18,12 @@ function CardInfo() {
                 <img
                   className="card-img-top"
                   src={product.image}
-                  alt="Card image cap"
+                  alt={product.title}
                 />
 
                 <div className="card-body">
-                  <h5 className="card-title">{product.title}</h5>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
+                  <h5 className="card-title">{product.category}</h5>
+                  <p className="card-text">{product.title}</p>
                   <a href="/#" className="btn btn-primary">
                     Go somewhere
                   </a>
