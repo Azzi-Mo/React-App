8d6bf11aa814a67a1e98545949c9849040bbef27
Azzi-Mo <alhwarymostafa@gmail.com>
2022-12-25 12:44:12 +0200

    edit

--- session-2 Simple Task/src/Componenet/Cardinfo.js
+++ session-2 Simple Task/src/Componenet/Cardinfo.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import { useEffect, useState } from "react";
 
 function CardInfo() {
@@ -9,30 +10,32 @@ function CardInfo() {
   }, []);
   return (
     <>
-      {products.map((product) => {
-        return (
-          <>
-            <div className="card" style={{ width: "18rem" }} key={product.id}>
-              <imag
-                className="card-img-top"
-                src={product.imag}
-                alt="Card image cap"
-              />
+      <section>
+        {products.map((product) => {
+          return (
+            <>
+              <div className="card" style={{ width: "18rem" }} key={product.id}>
+                <img
+                  className="card-img-top"
+                  src={product.image}
+                  alt="Card image cap"
+                />
 
-              <div className="card-body">
-                <h5 className="card-title">{product.title}</h5>
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <a href="/#" className="btn btn-primary">
-                  Go somewhere
-                </a>
+                <div className="card-body">
+                  <h5 className="card-title">{product.title}</h5>
+                  <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                  <a href="/#" className="btn btn-primary">
+                    Go somewhere
+                  </a>
+                </div>
               </div>
-            </div>
-          </>
-        );
-      })}
+            </>
+          );
+        })}
+      </section>
     </>
   );
 }
