c974986e3047a461960802c32423ba3c3b33d741
Azzi-Mo <alhwarymostafa@gmail.com>
2022-12-25 09:25:05 +0200

    use HOOKS at FuncComponent

--- session-1/src/Componenet/FuncComponent.js
+++ session-1/src/Componenet/FuncComponent.js
@@ -1,7 +1,13 @@
+import { useState } from "react"
+
 function MyApp(props){
+    let [num , setNum] = useState(10)
     return(
 
-        <h1>Hello {props.title} </h1>
+      <>
+      <h1>Hello {props.title} </h1>
+      <h1>{num}</h1>
+      </>
     )
 }
 export default MyApp
