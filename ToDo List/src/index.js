1d252c02deb63bc71048e6c24820d499c6cf01fb
Azzi-MO <alhwarymostafa@gmail.com>
2022-12-25 22:46:08 -0800

    edit

--- ToDo List/src/index.js
+++ ToDo List/src/index.js
@@ -2,18 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
-import NavBar from './Componenet/NavBar';
-import Slider from './Componenet/Slider';
-import './Componenet/Css Style/MainStyle.css'
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode> 
-  <section className='contain'>
-  <NavBar />
-   <Slider/>
-   </section>
+
  
 
   </React.StrictMode>
