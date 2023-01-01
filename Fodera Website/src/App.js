62fc0f24c3ef50755f3a70fd8688914db72eca3f
Azzi-Mo <alhwarymostafa@gmail.com>
2023-01-01 10:08:27 +0200

    add fodera again

---
+++ Fodera Website/src/App.js
@@ -0,0 +1,14 @@
+
+import "./App.css";
+import Home from "./Component/Home/Home";
+import Navs from "./Component/Navs/Nav";
+function App() {
+  return (  
+    <>
+    <Navs/>
+    <Home/>
+    </>
+  )
+}
+
+export default App;
---
+++ Fodera Website/src/Component/Home/Home.js
@@ -0,0 +1,14 @@
+import Header from "./Header";
+import "../Css_Style/Home.css";
+import SecNumber from "./SecNumber";
+import Pride from "./Pride";
+function Home() {
+  return (
+    <>
+      <Header />
+      <SecNumber/>
+      <Pride/>
+    </>
+  );
+}
+export default Home;
