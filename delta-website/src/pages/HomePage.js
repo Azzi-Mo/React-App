90dfc19bb7e49b0dfac06a9b0c445786cd68f086
Azzi-MO <alhwarymostafa@gmail.com>
2023-01-09 08:19:34 +0200

    edit

--- delta-website/src/pages/HomePage.js
+++ delta-website/src/pages/HomePage.js
@@ -1,9 +1,19 @@
 import React from "react";
-import Hero from "../component/Hero/Hero";
+import { Content } from "../component/Content/Content";
+import Features from '../component/Features/Features'
+import Hero from "../component/Hero/Hero"; 
+import {heroOne , heroTwo , heroThree} from '../data/HeroData'
 const HomePage = () => 
 {
     return (
-       <Hero />
+       <>
+        <Hero />
+       <Features />
+       <Content {...heroOne}/>
+       <Content {...heroTwo}/>
+       <Content {...heroThree}/>
+
+       </>
     )
 }
 export default HomePage
