0225b5b98f317c6126aa32c4200c262eda315954
Azzi-MO <alhwarymostafa@gmail.com>
2023-01-09 18:19:05 +0200

    edit

--- styled-components/src/App.js
+++ styled-components/src/App.js
@@ -1,7 +1,16 @@
-// import React from "react";
-
+import React from "react";
+import HomePage from "./Pages/HomePages";
+import GlobalStyle from './globalStyles';
+import { BrowserRouter as Router , Switch , Route} from "react-router-dom";
 function App() {
-  return 
+  return <Router>
+
+    <GlobalStyle />
+    <Switch>
+      <Route path='/' exact component={HomePage}/>
+    </Switch>
+
+  </Router>
 }
 
 export default App;
---
+++ styled-components/src/Pages/HomePages.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HomePages() {
+  return (
+    <div>
+       hello home 
+    </div>
+  )
+}
+
+export default HomePages
