252e05757cd58802361fb10de1c0cc9296df6565
Azzi-MO <alhwarymostafa@gmail.com>
2023-01-09 17:12:52 +0200

    edit

--- styled-components/src/App.js
+++ styled-components/src/App.js
@@ -1,16 +1,7 @@
 import React from "react";
-import { Container, Header } from "./GlobalStyles";
-import { ThemeProvider } from "styled-components";
-
 const theme = { color: "red" };
 function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <Container>
-        <Header color={theme} >this is header</Header>
-      </Container>
-    </ThemeProvider>
-  );
+  return 
 }
 
 export default App;
--- styled-components/src/GlobalStyles.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Container = styled.div`
-  border: 1px solid red;
-`;
-
-export const Header = styled.h1`
-  color: ${({ color }) => (color ? color : "blue")};
-`;
