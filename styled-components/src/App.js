4d4e798aa633b90197af94537174538f30dffe7f
Azzi-Mo <alhwarymostafa@gmail.com>
2023-01-10 07:26:14 +0200

    edit

--- styled-components/src/App.js
+++ styled-components/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import HomePage from "./Pages/HomePages";
 import GlobalStyle from './globalStyles';
+import Navbar from './Component/Navbar/Navbar'
 import { BrowserRouter as Router , Switch , Route} from "react-router-dom";
 function App() {
   return <Router>
 
     <GlobalStyle />
+    <Navbar />
     <Switch>
       <Route path='/' exact component={HomePage}/>
     </Switch>
