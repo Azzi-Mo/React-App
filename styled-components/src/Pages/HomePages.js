0b86a014d0e0e7e0070d65f4149149cb9cce0d3d
Azzi-Mo <alhwarymostafa@gmail.com>
2023-01-10 07:48:52 +0200

    edit

--- styled-components/src/Pages/HomePages.js
+++ styled-components/src/Pages/HomePages.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Features from '../Component/Features/Features'
+import Client from '../Component/Clients/Clients'
+
 import Hero from '../Component/Hero/Hero'
 
 function HomePages() {
   return <>
   <Hero />
   <Features />
+  <Client/>
   </>
 }
 
