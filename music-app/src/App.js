f6e024884e605d481667b874edd5b8370f47d675
Azzi-MO <alhwarymostafa@gmail.com>
2023-01-12 19:06:02 +0200

    edit

--- music-app/src/App.js
+++ music-app/src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router , Switch , Route, BrowserRouter } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import Signup from "./pages/SignuoPage/Signup";
 function App() {
   return (
    <>
+<Router>
+  <Switch>
+    <Route path='/' exact component={HomePage}/>
 
+    <Route path='/singnup' exact component={Signup}/>
+
+  </Switch>
+</Router>
    </>
   );
 }
