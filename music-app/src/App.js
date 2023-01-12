98bf244ee457d0feae3eec6f446b046587bcf08e
Azzi-MO <alhwarymostafa@gmail.com>
2023-01-12 20:22:17 +0200

    edit

--- music-app/src/App.js
+++ music-app/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter as Router , Switch , Route, BrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import Signup from "./pages/SignupPage/Signup";
+import GlobalStyle from "./globalStyle";
 function App() {
   return (
    <>
 <Router>
   <Switch>
+  <GlobalStyle />
     <Route path='/' exact component={HomePage}/>
 
     <Route path='/sign-up' exact component={Signup}/>
--- music-app/src/globalStyle.js
+++ music-app/src/globalStyle.js
@@ -121,4 +121,15 @@ export const Button = styled(motion.button)`
   white-space: nowarp;
   padding: ${({ big }) => (big ? "12px 64px" : "10px 20px")};
   color: #fff;
+  font-size:${({fontBig})=> fontBig ? '20px' : '16px'};
+  outline:none;
+  border:none;
+  cursor:pointer;
+  &:hover {
+    transition :all 0.3s ease-out;
+    color:black;
+    backgrond-color:#ebc214;
+  }
 `;
+
+export default GlobalStyle
