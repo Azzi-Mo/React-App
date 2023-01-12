220e4ed2d1646a7cd062094c4ac6bb133b461328
Azzi-MO <alhwarymostafa@gmail.com>
2023-01-12 19:09:25 +0200

    edit

--- music-app/src/App.js
+++ music-app/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router , Switch , Route, BrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
-import Signup from "./pages/SignuoPage/Signup";
+import Signup from "./pages/SignupPage/Signup";
 function App() {
   return (
    <>
