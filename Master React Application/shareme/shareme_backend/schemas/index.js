f2a03040fdb4ae50b5448e21530b17eb086bdddf
Azzi-MO <alhwarymostafa@gmail.com>
2023-01-18 17:07:58 +0200

    edit

--- Master React Application/shareme/shareme_backend/schemas/index.js
+++ Master React Application/shareme/shareme_backend/schemas/index.js
@@ -1,1 +1,7 @@
-export const schemaTypes = []
+import user from './user'
+import commnet from './commnet'
+import pin from './pin'
+import postedBy from './postedBy'
+import save from './save'
+
+export const schemaTypes = [user, commnet , pin , postedBy , save]
