06e580150907cc9dc68728265c741a3c98e8bac9
Azzi-MO <alhwarymostafa@gmail.com>
2023-01-18 10:33:01 +0200

    ecommerce app

---
+++ ECommerce/ecommerce-react/ecommerce/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'eb6yzqv5',
+    dataset: 'production'
+  }
+})
--- styled-components/src/App.js
+++ styled-components/src/App.js
@@ -12,7 +12,7 @@ function App() {
     <Switch>
       <Route path='/' exact component={HomePage}/>
     </Switch>
-    <Footer/>
+    <Footer/> 
 
   </Router>
 }
