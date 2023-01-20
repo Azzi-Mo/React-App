d7c4f52c6ab6bc5837b43dd95bffd8ec8c09fef2
Azzi-MO <alhwarymostafa@gmail.com>
2023-01-20 17:18:59 +0200

    edit

---
+++ ECommerce/ecommerce-react/lib/client.js
@@ -0,0 +1,10 @@
+import { SanityClient } from "@sanity/client";
+import  ImageUrlBuilder  from "@sanity/image-url";
+
+const  clint = sanityClient({
+    project:'',
+    dataset:'',
+    apiVersion:'',
+    useCd:'',
+    toker:''
+})
