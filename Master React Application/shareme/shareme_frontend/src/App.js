efd772e12fad6f75900bf06a3844e55ff4a209a2
Azzi-Mo <alhwarymostafa@gmail.com>
2023-01-21 13:30:09 +0200

    edit

--- Master React Application/shareme/shareme_frontend/src/App.js
+++ Master React Application/shareme/shareme_frontend/src/App.js
@@ -3,7 +3,6 @@ import { Routes, Route, useNavigate } from "react-router-dom";
 import Home from "./container/Home";
 import Login from "./component/Login";
 
-
 const App = () => {
   return (
     <Routes>
--- Master React Application/shareme/shareme_frontend/src/component/Login.jsx
+++ Master React Application/shareme/shareme_frontend/src/component/Login.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-
+import React from "react";
+import GoogleLogin from "react-google-login";
+import { useNavigate } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
+import shareVideo from "../assets/share.mp4";
+import logo from "../assets/logowhite.png";
 function login() {
   return (
-    <></>
-  )
+    <div className="flex justify-start items-center flex-col h-screen">
+      <div className="relative w-full h-full">
+        <video
+          src={shareVideo}
+          type="video/mp4"
+          muted
+          loop
+          autoPlay
+          controls={false}
+          className="w-full h-full object-cover"
+        />
+      </div>
+    </div>
+  );
 }
 
-export default login
+export default login;
