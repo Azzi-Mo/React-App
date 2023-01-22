c08d2acaa92f31c2a3cac2809d65f297aaba7dd8
Azzi-Mo <alhwarymostafa@gmail.com>
2023-01-22 14:15:09 +0200

    edit

--- Landing pages/Foodera Website/src/Component/Navs/Nav.js
+++ Landing pages/Foodera Website/src/Component/Navs/Nav.js
@@ -10,30 +10,7 @@ const Navs = () => {
   return (
     <section className="NavBar">
 
-      <Navbar bg="light" expand="lg">
-
-        <Container className="container">
-          <Navbar.Brand href="#home">
-            <img src={logo} className='logo' alt="logo"></img>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home"></Nav.Link>
-            </Nav>
-
-            <Nav>
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">About Us</Nav.Link>
-              <Nav.Link href="#link">Explore Foods</Nav.Link>
-              <Nav.Link href="#link">Reviews</Nav.Link>
-              <Nav.Link href="#link">FAQ</Nav.Link>
-              <Nav.Link href="#link">1800789123</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-
+     
     </section>
   );
 };
