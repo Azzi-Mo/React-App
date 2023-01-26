244906a48fe63d70510571b507f8ff748c239f40
Azzi-Mo <alhwarymostafa@gmail.com>
2023-01-26 15:10:04 +0200

    edit

--- Simple idea/dojo-blog/src/Home.js
+++ Simple idea/dojo-blog/src/Home.js
@@ -5,27 +5,27 @@ import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState([]);
   const [isPending, setPending] = useState(true);
-
-  const [name, setName] = useState("mojo");
-
-  // const DeletBlogs = (id) => {
-  //   const newBlog = blogs.filter((blog) => blog.id !== id);
-  //   setBlogs(newBlog);
-  // };
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     setTimeout(() => {
       fetch("http://localhost:8000/blogs")
-        .then((res) => res.json())
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("could not fetch the data for that resource");
+          }
+          res.json();
+        })
         .then((data) => {
           setBlogs(data);
           setPending(false);
         })
-        .catch((err) => console.log(err.message));
+        .catch((err) => setError(err.message));
     }, 1000);
   }, []);
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isPending && <div>loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
