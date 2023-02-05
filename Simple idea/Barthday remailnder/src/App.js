0ade6f7b09b0e52ac437a8ffd5cb07b8590c2b4b
Azzi-Mo <alhwarymostafa@gmail.com>
2023-02-05 15:02:54 +0200

    add Birthday Reminder

---
+++ Simple idea/Barthday remailnder/src/App.js
@@ -0,0 +1,14 @@
+
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import PeopelProfile from "./component/PeopelProfile";
+
+function App() {
+  return (
+    <Router>
+      <PeopelProfile/>
+    </Router>
+  );
+}
+
+export default App;
