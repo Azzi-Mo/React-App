43f779428d4fa9449677d4b4ad34af5ce103fbac
Azzi-Mo <alhwarymostafa@gmail.com>
2023-02-07 19:49:28 +0200

    edit

--- Simple idea/dojo-blog/src/Home.js
+++ Simple idea/dojo-blog/src/Home.js
@@ -1,44 +1,45 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
-// import useFetch from "./useEffect";
+import useFetch from "./useEffect";
 const Home = () => {
-  // const {
-  //   data: blogs,
-  //   isPending,
-  //   error,
-  // } = useFetch("http://localhost:8000/blogs");
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
   // const handelDelet = (id) => {};
 
-  const [blogs, setBlogs] = useState([]);
-  const [isPending, setPending] = useState(true);
-  const [error, setError] = useState(null);
+  // const [blogs, setBlogs] = useState([]);
+  // const [isPending, setPending] = useState(true);
+  // const [error, setError] = useState(null);
 
-  useEffect(() => {
-    setTimeout(
-      () =>
-        fetch("http://localhost:8000/blogss")
-          .then((res) => 
-          {  if(!res.ok)
-            {
-              throw Error('could not fetch the data for that resource')
-            }
+  // useEffect(() => {
+  //   setTimeout(
+  //     () =>
+  //       fetch("http://localhost:8000/blogss")
+  //         .then((res) => 
+  //         {  if(!res.ok)
+  //           {
+  //             throw Error('could not fetch the data for that resource')
+  //           }
     
-            return res.json()
-            //  console.log(res);
-          }
-            )
-          .then((data) => {
-            setBlogs(data);
-            setPending(false);
-            setError(null)
-          })
-          .catch((error) => {
-            setPending(false)
-            setError(error.message)
-          }),
-      1000
-    );
-  }, []);
+  //           return res.json()
+  //           //  console.log(res);
+  //         }
+  //           )
+  //         .then((data) => {
+  //           setBlogs(data);
+  //           setPending(false);
+  //           setError(null)
+  //         })
+  //         .catch((error) => {
+  //           setPending(false)
+  //           setError(error.message)
+  //         }),
+  //     1000
+  //   );
+  // }, []);
+
   // const handelDelet = (id) => {
   //   const newBlogs = blogs.filter((blog) => blog.id !== id);
   //   setBlogs(newBlogs);
--- Simple idea/dojo-blog/src/useEffect.js
+++ Simple idea/dojo-blog/src/useEffect.js
@@ -1,41 +1,31 @@
-import { useEffect , useState} from "react";
+import { useEffect, useState } from "react";
 
-const useFetch = (url) =>
-{
-    const [data, setdata] = useState(null);
-    const [isPending, setPending] = useState(true);
-    const [error, setError] = useState(null);
-  
-    useEffect(() => {
+const useFetch = (url) => {
+  const [data, setdata] = useState(null);
+  const [isPending, setPending] = useState(true);
+  const [error, setError] = useState(null);
 
-        fetch(url)
-          .then((res) => {
-  
-            if (!res.ok) {
-              throw Error("could not fetch the data for the resourses");
-            }
-            return res.json();
-  
-          })
-  
-          .then((data) => {
-  
-            setdata(data);
-            setPending(false);
-            setError(null);
-  
-          })
-          .catch((error) => {
-  
-            setPending(false);
-            setError(error.message);
-  
-          });
-  
-    }, [url] );
+  useEffect(() => {
+    fetch(url)
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("could not fetch the data for the resourses");
+        }
+        return res.json();
+      })
 
+      .then((data) => {
+        setdata(data);
+        setPending(false);
+        setError(null);
+      })
+      .catch((error) => {
+        setPending(false);
+        setError(error.message);
+      });
+  }, [url]);
 
-    return {data , isPending , error } 
-}
+  return { data, isPending, error };
+};
 
-export default useFetch
+export default useFetch;
