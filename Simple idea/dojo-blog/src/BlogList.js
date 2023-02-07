010ff39f49e69a299ef1b60857abb60c4c4a69ec
Azzi-Mo <alhwarymostafa@gmail.com>
2023-02-07 18:26:02 +0200

    edit

--- Simple idea/dojo-blog/src/BlogList.js
+++ Simple idea/dojo-blog/src/BlogList.js
@@ -1,6 +1,6 @@
 import React from "react";
 const BlogList = (props) => {
-  const { blogs, title, handelDelet } = props;
+  const { blogs, title } = props;
   return (
     <div className="blog-list">
       <h2>{title}</h2>
@@ -10,7 +10,6 @@ const BlogList = (props) => {
             <div className="blog-preview" key={blog.id}>
               <h2>{blog.title}</h2>
               <p>written by {blog.author}</p>
-              <button onClick={() => handelDelet(blog.id)}>Delet</button>
             </div>
           </>
         );
