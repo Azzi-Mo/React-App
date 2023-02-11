66051c68084c22e51ebb310c710a4525746b6935
Azzi-Mo <alhwarymostafa@gmail.com>
2023-02-11 14:45:39 +0200

    add initial files

---
+++ Simple idea/tours/src/App.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+const url = "https://course-api.com/react-tours-project";
+export default function App() {
+  const [loading, setLoding] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoding(true);
+
+    try {
+        const response = await fetch(url);
+        const tours = await response.json();
+        setLoding(false)
+        setTours(tours)
+    }
+    catch(error)
+    {
+        setLoding(true)
+        console.log(error);
+    }
+    console.log(tours);
+  };
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  return (
+    <>
+      <main>
+        <Tours />
+      </main>
+    </>
+  );
+}
---
+++ Simple idea/tours/src/Tour.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Tour() {
+  return (
+    <h2>
+       single Tour Component
+    </h2>
+  )
+}
