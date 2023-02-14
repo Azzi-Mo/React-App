4c753ca6248cf6cef9a60334e746e83b6ae5df1b
Azzi-Mo <alhwarymostafa@gmail.com>
2023-02-14 18:47:02 +0200

    edit

--- Simple idea/Slider/src/App.js
+++ Simple idea/Slider/src/App.js
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
 import { FaQuoteRight } from "react-icons/fa";
 import data from "./data";
 function App() {
   const [people, setPeopel] = useState(data);
-  const [index , setIndex] = useState(0)
+  const [index, setIndex] = useState(0);
 
   return (
     <section className="section">
@@ -14,30 +14,31 @@ function App() {
         </h2>
       </div>
       <div className="section-center">
-      {people.map((person,personIndx)=>{
-         const {id , image , name , title , quote} = person
-        let position = ' nextSlide'
-        if(personIndx === index)
-        {
-          position  = 'activeSlide'
-        }
-        return ( 
-              <article className={position} key={id}>
-              <img src={image} className='person-img' alt={name}></img>
+        {people.map((person, personIndx) => {
+          const { id, image, name, title, quote } = person;
+          let position = " nextSlide";
+          if (personIndx === index) {
+            position = "activeSlide";
+          }
+          if (personIndx === index - 1 || (index === 0 && personIndx === people.length - 1) ) {
+            position = " lastSlide";
+          }
+          return (
+            <article className={position} key={id}>
+              <img src={image} className="person-img" alt={name}></img>
               <h4>{name}</h4>
               <p className="title">{title}</p>
               <p className="text">{quote}</p>
               <FaQuoteRight className="icon" />
-              </article>
-        )
-
-      })}
-      <button className="prev">
-      <FiChevronLeft />
-      </button>
-      <button className="next">
-      <FiChevronRight />
-      </button>
+            </article>
+          );
+        })}
+        <button className="prev">
+          <FiChevronLeft />
+        </button>
+        <button className="next">
+          <FiChevronRight />
+        </button>
       </div>
     </section>
   );
