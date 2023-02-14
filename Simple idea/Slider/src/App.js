4a6f0f326eccad457d81b880c2728d150d8f6c64
Azzi-Mo <alhwarymostafa@gmail.com>
2023-02-14 18:36:15 +0200

    edit

--- Simple idea/Slider/src/App.js
+++ Simple idea/Slider/src/App.js
@@ -1,12 +1,45 @@
 import React, { useEffect, useState } from "react";
-import { FaAngleDoubleRight } from "react-icons/fa";
-import data from './data'
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import data from "./data";
 function App() {
+  const [people, setPeopel] = useState(data);
+  const [index , setIndex] = useState(0)
 
-  const [people , setPeopel] = useState(data)
-  console.log(people);
   return (
-<></>
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>Reviews
+        </h2>
+      </div>
+      <div className="section-center">
+      {people.map((person,personIndx)=>{
+         const {id , image , name , title , quote} = person
+        let position = ' nextSlide'
+        if(personIndx === index)
+        {
+          position  = 'activeSlide'
+        }
+        return ( 
+              <article className={position} key={id}>
+              <img src={image} className='person-img' alt={name}></img>
+              <h4>{name}</h4>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon" />
+              </article>
+        )
+
+      })}
+      <button className="prev">
+      <FiChevronLeft />
+      </button>
+      <button className="next">
+      <FiChevronRight />
+      </button>
+      </div>
+    </section>
   );
 }
 
