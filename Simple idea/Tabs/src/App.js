f5d6a11c070f07531192969d86028631c68db24d
Azzi-Mo <alhwarymostafa@gmail.com>
2023-02-14 14:28:59 +0200

    edit

--- Simple idea/Tabs/src/App.js
+++ Simple idea/Tabs/src/App.js
@@ -1,9 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FaAngleDoubleRight } from "react-icons/fa";
 import dataInfo from "./data";
+
+const url = "https://course-api.com/react-tabs-project";
 function App() {
-  const [data, setData] = useState(dataInfo);
-  const { company, dates, duties, title } = data;
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const { company, dates, duties, title } = dataInfo;
+  console.log(duties);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (loading) {
+    return (
+      <section className="seciton loading">
+        <h2>loading...</h2>
+      </section>
+    );
+  }
   return (
     <section className="section">
       <div className="title">
@@ -11,7 +36,23 @@ function App() {
         <div className="underline"></div>
       </div>
       <div className="job-center">
-        <article className="job-info"></article>
+        {/* btn container */}
+        {/* job info */}
+
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-data">{dates}</p>
+
+          {duties.map((duty, indx) => {
+            return (
+              <div key={indx} className="job-desc">
+                <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+                <p>{duty}</p>
+              </div>
+            );
+          })}
+        </article>
       </div>
     </section>
   );
