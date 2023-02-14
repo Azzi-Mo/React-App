bc21c2730c2a10026eb89846f747e0fe43c26132
Azzi-Mo <alhwarymostafa@gmail.com>
2023-02-14 17:34:14 +0200

    intial project

---
+++ Simple idea/Slider/src/App.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
+import data from './data'
+function App() {
+
+  const [people , setPeopel] = useState(data)
+  console.log(people);
+  return (
+<></>
+  );
+}
+
+export default App;
