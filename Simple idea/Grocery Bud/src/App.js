55474528285a8003597d28b40c4905de11a9d099
Azzi-Mo <alhwarymostafa@gmail.com>
2023-02-18 21:14:06 +0200

    edit

--- Simple idea/Grocery Bud/src/App.js
+++ Simple idea/Grocery Bud/src/App.js
@@ -1,12 +1,23 @@
 import React, { useState } from "react";
 import Alert from "./Alert";
+import List from "./List";
+
 function App() {
   const [name, setName] = useState("");
-  const [aleart, setAleart] = useState({ show: true, msg: "", type: "" });
-  const [isEditing, setEditing] = useState(true);
+  const [list, setList] = useState([]);
+  const [isEditing, setisEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [aleart, setAleart] = useState({ show: false, msg: "", type: "" });
   const handelSubmit = (e) => {
     e.preventDefault();
-
+    if (!name) {
+      // Please Enter Value
+    } else if (name && isEditing) {
+    } else {
+      const newItems = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItems]);
+      setName("");
+    }
   };
   return (
     <section className="section-center" onSubmit={handelSubmit}>
@@ -14,17 +25,24 @@ function App() {
         {aleart.show && <Alert />}
         <h3> grocery bud</h3>
         <div className="form-control">
-          <input type="text" className="grocery" placeholder="g. eggs" />
-          <button
-            type="submit"
+          <input
             value={name}
             onChange={(e) => setName(e.target.value)}
-            className="submit-btn"
-          >
-            {isEditing ? "edite" : "submit"}
+            type="text"
+            className="grocery"
+            placeholder="g. eggs"
+          />
+          <button type="submit" className="submit-btn">
+            {isEditing ? "edit" : "submit"}
           </button>
         </div>
       </form>
+      {list.length > 0 && (
+        <div className="grocery-container">
+          <List items={list} />
+          <button className="clear-btn">clear items</button>
+        </div>
+      )}
     </section>
   );
 }
--- Simple idea/Grocery Bud/src/List.js
+++ Simple idea/Grocery Bud/src/List.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { FaEdit, FaTrash } from "react-icons/fa";
 
-export default function list() {
+export default function list({ items }) {
   return (
-    <div>
-      
+    <div className="grocery-list">
+      {items.map((item) => {
+        const { id, title } = item;
+        return (
+          <article key={id} className="grocery-item">
+            <p className="title">{title}</p>
+            <div className="btn-container">
+              <button tpye="button" className="edit-btn">
+                <FaEdit />
+              </button>
+              <button tpye="button" className="delete-btn">
+                <FaTrash />
+              </button>
+            </div>
+          </article>
+        );
+      })}
     </div>
-  )
+  );
 }
