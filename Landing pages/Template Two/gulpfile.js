1c47d50304aea222c41f5408892f9765ab3df226
Azzi-Mo <alhwarymostafa@gmail.com>
2023-03-30 01:48:06 +0200

    edit

--- Landing pages/Template Two/gulpfile.js
+++ Landing pages/Template Two/gulpfile.js
@@ -1,31 +1,58 @@
 /* eslint-disable no-undef */
 const gulp = require('gulp');
-const plumber = require('gulp-plumber');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 const babel = require('gulp-babel');
-const sourcemaps = require('gulp-sourcemaps');
-const rename = require('gulp-rename');
+const plumber = require('gulp-plumber');
+// import plumber from "gulp-plumber";
+// import babel from "gulp-babel";
+// import { init, write } from "gulp-sourcemaps";
+// import rename from "gulp-rename";
 
 // Set up the source and destination file paths
 const paths = {
-  src: ['./jsx/*.jsx' , './jsx/*.js'],
-  dest: './src/Component'
+  src: ["./jsx/*.jsx", "./jsx/*.js"],
+  dest: "./src/Component",
 };
 
 // Define the gulp task
 function watch() {
-  return gulp.watch(paths.src.map((src)=> src), compile);
+  gulp.watch(
+    paths.src.map((src) => src),
+    compile 
+    
+  );
+  gulp.watch(
+    './Style/Styles.scss',
+    compielSass 
+    
+  );
+}
+
+function compielSass()
+{
+  return gulp.src('./Style/Styles.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(postcss([autoprefixer()]))
+  .pipe(dest('./src/Component'));
 }
 
 // Define the function to compile the JSX file
 function compile() {
-  return gulp.src(paths.src.map((src)=> src))
+  return gulp.src(paths.src.map((src) => src))
     .pipe(plumber()) // Prevents gulp from stopping on error
-    .pipe(sourcemaps.init()) // Creates sourcemaps for debugging
+    .pipe(init()) // Creates sourcemaps for debugging
     .pipe(babel()) // Transpile JSX to ES5 JavaScript
-    .pipe(rename({ extname: '.js' })) // Rename the file extension
-    .pipe(sourcemaps.write('.')) // Writes the sourcemaps
-    .pipe(gulp.dest(paths.dest)); // Outputs the compiled file
+    .pipe(rename({ extname: ".js" })) // Rename the file extension
+    .pipe(write(".")) // Writes the sourcemaps
+    .pipe(dest(paths.dest)); // Outputs the compiled file
 }
 
-// Expose the gulp task for command line usage
+
+
+// Define the gulp task
+
+exports.styles = styles;
+exports.scripts = scripts;
 exports.watch = watch;
--- Landing pages/Template Two/jsx/NavBar.jsx
+++ Landing pages/Template Two/jsx/NavBar.jsx
@@ -1,55 +1,10 @@
 import React from "react";
-// import items from "./Data";
 
-// eslint-disable-next-line no-undef
-// const allCategories = [...new Set(items.map((item) => item.category))];
-// eslint-disable-next-line react-hooks/rules-of-hooks
-// const [categories] = useState(allCategories);
 
 const NavBar = () => {
-  // if (!categories.length) {
-  //   return null;
-  // }
 
-  return (
-    <section className="navbar container">
-      <div className="Logo">
-        <img
-          decoding="async"
-          className="logo"
-          src="./imgaes/logo.png"
-          alt="navbar logo"
-        />
-      </div>
-      <div className="icon">
-        <i className="fa-solid fa-bars"></i>
-
-        <ul>
-          <li>
-            <a href="#services">Services</a>
-          </li>
-          <li>
-            <a href="#portfolio">Portfolio</a>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
-          <li>
-            <a href="#about">About</a>
-          </li>
-        </ul>
-
-        {/* <ul>
-          {categories.map((categorie, indx) => {
-            return (
-              <li key={indx}>
-                <a>{categorie}</a>
-              </li>
-            );
-          })}
-        </ul> */}
-      </div>
-    </section>
+  return ( 
+<></>
   );
 };
 
